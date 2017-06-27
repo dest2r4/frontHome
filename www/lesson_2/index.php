30fe3abce96fd8c304751970dbd7b2a73ade6994
Alex <alkhonko@gmail.com>
2017-06-27 19:12:49 +0300

    second lesson

---
+++ www/lesson_2/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Document</title>
+</head>
+<body>
+<section id="top_Menu">
+<section id="home_btn">
+<h1><a href="index.php" >Jet<span id="jetro">ro</span></a></h1>
+</section>
+<nav>
+	<ol >
+		<li><a href="#" id="first">home</a></li>
+		<li><a href="#">about us</a></li>
+		<li><a href="#">portfolio</a></li>
+		<li><a href="#">contact us</a></li>
+
+	</ol>
+</nav>
+</section>
+<header>
+	<ol>
+		<li class="percent_20"><a href="#" ><img src="img/legtArrow.png"   class="arrow"  alt=""></a></li>
+		<li> <div id="lorem_header">
+			<h1>slide 1</h1>
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  "</p>
+		</div>	</li>
+		<li class="percent_20" ><a href="#" ><img src="img/rightArrow.png"  class="arrow" alt=""></a></li>
+	</ol>
+</header>
+
+<main>
+	<section id="top_main_nav">
+		<ul>
+			<li><img src="img/img1.jpg" alt=""></li>
+			<li><img src="img/img2.jpg" alt=""></li>
+			<li><img src="img/img3.jpg" alt=""></li>
+			<li><img src="img/img4.jpg" alt=""></li>
+			<li><img src="img/img5.jpg" alt=""></li>
+			<li><img src="img/img6.jpg" alt=""></li>
+		</ul>
+	</section>
+	<section id="article_part">
+		
+		<article>
+			<img src="img/si1.png" alt="">
+			<h3>check out my latest portfolio items</h3>
+			<p>Maecenas a orci consectetur, sodales tellus sed, rhoncus turpis. Fusce congue diam vitae ex faucibus iaculis. Phasellus ac efficitur mi. Suspendisse at sapien vel enim laoreet posuere et tincidunt augue.</p>
+			<button>
+				<a href="#">more</a>
+			</button>
+		</article>
+		<article>
+			<img src="img/si2.png" alt="">
+			<h3> offer top notch quality work &amp; services</h3>
+			<p>Maecenas a orci consectetur, sodales tellus sed, rhoncus turpis. Fusce congue diam vitae ex faucibus iaculis. Phasellus ac efficitur mi. Suspendisse at sapien vel enim laoreet posuere et tincidunt augue.</p>
+			<button>
+				<a href="#">more</a>
+			</button>
+		</article>
+		<article>
+			<img src="img/si3.png" alt="">
+			<h3>follow me on twitter facebook &amp; pinterest</h3>
+			<p>Maecenas a orci consectetur, sodales tellus sed, rhoncus turpis. Fusce congue diam vitae ex faucibus iaculis. Phasellus ac efficitur mi. Suspendisse at sapien vel enim laoreet posuere et tincidunt augue.</p>
+			<button>
+				<a href="#">more</a>
+			</button>
+
+		</article>
+	</section>
+	<section id="recent_work">
+	<section>
+	<h2>recent works</h2>
+	<p></p>
+</section>
+	<article id="first">
+		<a href="#"><img src="img/ti1.jpg" alt=""></a>
+		<h4></h4>
+		<p></p>
+	</article>
+	<article>
+		<a href="#"><img src="img/ti2.jpg" alt=""></a>
+		<h4></h4>
+		<p></p>
+	</article>
+	<article>
+		<a href="#"><img src="img/ti3.jpg" alt=""></a>
+		<h4></h4>
+		<p></p>
+	</article>
+	<article id="last">
+		<a href="#"><img src="img/ti4.jpg" alt=""></a>
+		<h4></h4>
+		<p></p>
+	</article>
+		
+	</section>
+</main>
+<footer>
+	
+</footer>
+	
+</body>
+</html>
