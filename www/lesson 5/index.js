6b46111195e3d7d000f1abb1b971dc1769a0afef
Alex <alkhonko@gmail.com>
2017-07-18 13:18:19 +0300

    modified

--- www/lesson 5/index.js
+++ www/lesson 5/index.js
@@ -7,30 +7,47 @@ var q="<br>";
 var a=prompt("Первый член");
 var b=prompt("К-во итераций");
 var c=prompt("Множитель");
+var look=confirm("Посмотреть результат всех  итераций цикла?");
+(look==true)?look=1:look=0;
+
 //само решение
-(y==1)?Arith(a,b,c):Math(a,b,c);
+(y==1)?Arith(a,b,c,look):Math(a,b,c,look);
 // a - первый член b - кво элем с-сколько добавить
-function Math( a, b=1, c=1){
+function Math( a, b=1, c=1,l){
 	var result=0;
 	a=parseInt(a);
 	b=parseInt(b);
 	c=parseInt(c);
+	l=parseInt(l);
+	if(l==1){
 	for (var i = 0; i < b; i++) {
 		 
 		 result=result+(a+c);
-		 document.write(result+q);
+		 document.write(i+" ==> "+result+q);
 		 
 		}	
-
+	}
+	else{
+		for (var i = 0; i < b; i++) {
+		 
+		 result=result+(a+c);
+		 
+		 
+		}
+		document.write (b+" --> "+result);	
+	}
+	
 
 }
 //Math(a,b,c);
 
-function Arith( a, b=1, c=1){
+function Arith( a, b=1, c=1,l){
 	var result=true;
 	a=parseInt(a);
 	b=parseInt(b);
 	c=parseInt(c);
+	l=parseInt(l);
+	if(l==1){
 	if (a==0|| b==0||c==0) {
 		document.write ("НЕЛЬЗЯ множить на 0");
 	}
@@ -38,9 +55,21 @@ function Arith( a, b=1, c=1){
 	for (var i = 0; i < b; i++) {
 		 
 		 result=result*(a*c);
-		 document.write(result+q);
+		 document.write(i+" ==> "+result+q);
 		 
 		}	
+	}}
+	else{
+		if (a==0|| b==0||c==0) {
+		document.write ("НЕЛЬЗЯ множить на 0");
 	}
+	else{
+	for (var i = 0; i < b; i++) {
+		 
+		 result=result*(a*c);
+		}	
+		document.write (b+" --> "+result);
+	}}
+
 
 }
